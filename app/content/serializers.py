0b07515de4be7f29f30c5f210403e991f6528ef6
saveliigeorgiev <savgeorgiev17@gmail.com>
2025-02-22 18:48:51 +0000

    Contract added and an endpoint to create it

---
+++ app/content/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Contract
+
+from typing import Any, Dict
+class ContractSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contract
+        fields = ["id", "title", "reward", "description", "contractor", "contractee", "mediator", "completed",
+                  "transaction_hash"]
+        read_only_fields = ["contractor"]  # Prevent users from setting the contractor manually
+
+    def create(self, validated_data):
+        request = self.context.get("request")
+        if request and hasattr(request, "user"):
+            validated_data["contractor"] = request.user  # Assign contractor automatically
+
+        return super().create(validated_data)
